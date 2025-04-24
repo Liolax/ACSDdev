c7db4e2a0c52e70aee880af5cc6b251aeaa8a033
Liolax <donleonkot@gmail.com>
2025-04-24 18:34:48 +0100

    Fixed Contact Page SCSS

--- frontend/src/pages/ContactPage.js
+++ frontend/src/pages/ContactPage.js
@@ -4,6 +4,7 @@ import Header from '../components/layouts/Header';
 import Footer from '../components/layouts/Footer';
 import ContactForm from '../components/forms/ContactForm';
 import LoginPopup from '../components/LoginPopup';
+import '../assets/styles/pages/_contact.scss';
 import '../assets/styles/components/_contact-form.scss';
 
 const ContactPage = () => {
